480bf39b273928bad9488dd91d5433fb8675215e
Syed Shahid <syedshahid.9380@gmail.com>
2026-07-21 23:07:11 +0530

    Update API endpoint for login authentication

--- src/routes/auth.login.tsx
+++ src/routes/auth.login.tsx
@@ -41,7 +41,7 @@ function LoginPage() {
     
     try {
       // ALWAYS hit the single Master API endpoint
-      const res = await fetch("http://localhost:5000/api/auth/login", {
+      const res = await fetch("https://bharat-career-connect.onrender.com//api/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ 
